8545526a5399122ce24485f8652273fc4fa8a70f
kaylahoo <790970276@qq.com>
2023-05-23 21:13:25 +0800

    saa

--- src/networks.py
+++ src/networks.py
@@ -227,7 +227,7 @@ class InpaintGenerator1(BaseNetwork):
 
         self.encoder = nn.Sequential(
             nn.ReflectionPad2d(3),
-            spectral_norm(nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, padding=0), use_spectral_norm),
+            spectral_norm(nn.Conv2d(in_channels=4, out_channels=64, kernel_size=7, padding=0), use_spectral_norm),
             nn.InstanceNorm2d(64, track_running_stats=False),
             nn.ReLU(True),
 
