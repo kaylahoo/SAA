1a00a1464d57a09ac54bd401b2091e6275f49938
kaylahoo <790970276@qq.com>
2023-05-25 12:34:33 +0800

    saa

--- src/networks.py
+++ src/networks.py
@@ -288,10 +288,9 @@ class InpaintGenerator1(BaseNetwork):
 
         path ="/home/lab265/lab265/lab508_8T/liulu/SAA/checkpoints/ffhq/InpaintingModel_gen.pth"
         self.content_codec = InpaintGenerator(ckpt_path=path, trainable=False)
-        # self.attn_k = self.content_codec.quantize.get_codebook()['default']['code']
-        # self.attn_v = self.content_codec.quantize.get_codebook()['default']['code']
-        #
-        # self.attn = nn.MultiheadAttention(embed_dim=512, num_heads=8)
+        self.kv = self.content_codec.quantize.get_codebook()['default']['code']
+
+        self.attn = nn.MultiheadAttention(embed_dim=512, num_heads=8)
 
 
 
@@ -300,12 +299,12 @@ class InpaintGenerator1(BaseNetwork):
         x = images_masked
         x = self.encoder(x)
         x = self.middle1(x)
-        # b, c, h, w = x.shape
-        # x = x.reshape(b, c, h * w).permute(2, 0, 1).contiguous()
+        b, c, h, w = x.shape
+        tgt = x.reshape(b, c, h * w).permute(2, 0, 1).contiguous()
         # #print(tgt.shape) [1024,12,512]
-        # mem = self.codebook.repeat(x.size(0), 1, 1).to(tgt.device)
-        # attn_out, _ = self.attn(x, mem, mem)
-        # x = x + attn_out
+        mem = self.kv.repeat(1, x.shape[0], 1).to(tgt.device)
+        attn_out, _ = self.attn(x, mem, mem)
+        x = x + attn_out
         x = self.middle2(x)
         x = self.decoder(x)
         x = torch.sigmoid(x)
